c9515ea4b1e205cc76c2c2548054d2c48dcabd32
Deepak Dhull <dpkdhull3@gmail.com>
2018-11-20 21:28:16 +0530

    Pages Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'landing',
+    pathMatch: 'full'
+  },
+  {
+    path: 'landing',
+    loadChildren: './pages/landing/landing.module#LandingPageModule',
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'verify',
+    loadChildren: './pages/sign-in/sign-in.module#SignInPageModule'
+  },
+  { path: 'landing', loadChildren: './pages/landing/landing.module#LandingPageModule' }
 ];
 
 @NgModule({
